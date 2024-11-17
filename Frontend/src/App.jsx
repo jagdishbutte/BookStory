22df1880f8b6fdca1bf49675f7ea2e4e5383f4c9
jagdishbutte <jagdishbutte@gmail.com>
2024-11-17 08:27:04 +0530

    Project setup and Navbar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <>
+      <div>
+        <div>Left</div>
+        <div>Right</div>
+      </div>
+    </>
+  );
+}
+
+export default Banner;
