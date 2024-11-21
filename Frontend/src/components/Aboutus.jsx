8f9f6827939bf32a55cbb7b7ffe4f1ab359f0a11
jagdishbutte <jagdishbutte@gmail.com>
2024-11-21 12:43:43 +0530

    About Us Page Updates (Frontend Final)

--- Frontend/src/components/Aboutus.jsx
+++ Frontend/src/components/Aboutus.jsx
@@ -1,36 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Aboutus() {
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col space-y-6 md:flex-row">
-        <div className="w-full md:w-1/2 mt-12 md:mt-32 order-2 md:order-1">
-          <div className="space-y-6 md:mr-6">
-            <h1 className="text-4xl font-bold">
-              Wha is a BookStory?
-              <br />
-              <span className="text-green-500">Way to learn something new everyday!!!</span>
-            </h1>
-            <p className="text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              voluptatibus nemo quos eveniet neque autem, ullam maiores magni
-              quam fugiat.
-            </p>
-            <hr />
-            <p className="text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eius pariatur iure veniam nemo sunt cum impedit numquam inventore iusto! Saepe similique autem quibusdam in sunt. Voluptatem exercitationem officia fuga?
-            </p>
-          </div>
-          <button className="mt-6 bg-green-500 hover:bg-green-400 px-4 py-2 rounded-md duration-300">
-            Subscribe
-          </button>
-        </div>
-        <div className="w-full md:w-1/2 order-1">
-          <img
-            src="banner.jpg"
-            alt="BookStory"
-            className="h-92 w-92 mt-20 md:mt-20 rounded-lg"
-          />
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-900 dark:text-white">
+        <div className="text-center items-center justify-center">
+          {/* Unwanted component just to adjust the view  */}
+          <h1 className="text-2xl md:text-4xl font-semibold">
+            <span className="text-white">.</span>
+          </h1>
+
+          <h1 className="mt-20 text-2xl md:text-4xl font-semibold">What is a{" "} <span className="text-green-500">BookStory?</span></h1>
+          <p className="mt-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            a doloremque quos, dolores optio temporibus minima sed culpa saepe,
+            quidem hic aperiam, porro facilis expedita neque libero! Quod optio
+            tempore facilis consequatur, maiores voluptate itaque laudantium
+            culpa veritatis. Deserunt, eius. Nobis provident inventore vitae
+            praesentium sed perferendis laboriosam neque tenetur.
+          </p>
+          <Link to={"/"}>
+            <button className="mt-6 bg-green-500 hover:bg-green-700 px-4 py-2 rounded-md duration-300">
+              Go to Home
+            </button>
+          </Link>
         </div>
       </div>
     </>
